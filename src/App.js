c9229efb30872601a56b618213211d1416ad2bf1
quisethecoder <marquise_dawkins@aol.com>
2023-01-03 01:15:50 -0500

    euqal case set up

--- src/App.js
+++ src/App.js
@@ -56,6 +56,18 @@ function reducer(state, {type, payload}){
 
       case ACTIONS.CLEAR:
         return {}
+
+      case ACTIONS.EVALUATE:
+        if (state.operation == null || state.currentOperand == null || state.previousOperand == null){
+          return state
+        }
+
+        return{
+          ...state,
+          previousOperand: null,
+          operation: null,
+          currentOperand: evaluate(state),
+        }
   }
 }
 
@@ -107,7 +119,7 @@ function App(){
       <OperationButton operation="-" dispatch={dispatch} /> 
       <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
-      <button className="span-two">=</button>
+      <button className="span-two" onClick={() => dispatch({type: ACTIONS.EVALUATE})}>=</button>
     </div>
   )
 }
