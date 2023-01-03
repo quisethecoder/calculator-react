26d8bdc7b3e8076059bfcf6fe5c9b22abf0076f8
quisethecoder <marquise_dawkins@aol.com>
2023-01-03 00:31:27 -0500

    button function

--- src/App.js
+++ src/App.js
@@ -31,22 +31,19 @@ function App(){
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
-      <OperationButton digit="÷" dispatch={dispatch} /> 
+      <OperationButton operation="÷" dispatch={dispatch} /> 
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
       <DigitButton digit="3" dispatch={dispatch} />
-      <OperationButton digit="x" dispatch={dispatch} /> 
-      <button>x</button>
+      <OperationButton operation="x" dispatch={dispatch} /> 
       <DigitButton digit="4" dispatch={dispatch} />
       <DigitButton digit="5" dispatch={dispatch} />
       <DigitButton digit="6" dispatch={dispatch} />
-      <OperationButton digit="+" dispatch={dispatch} /> 
-      <button>+</button>
+      <OperationButton operation="+" dispatch={dispatch} /> 
       <DigitButton digit="7" dispatch={dispatch} />
       <DigitButton digit="8" dispatch={dispatch} />
       <DigitButton digit="9" dispatch={dispatch} />
-      <OperationButton digit="-" dispatch={dispatch} /> 
-      <button>-</button>
+      <OperationButton operation="-" dispatch={dispatch} /> 
       <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
       <button className="span-two">=</button>
