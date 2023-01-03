4a69ddfc12f46ea228af2de56125c1f0c3ba8dbe
quisethecoder <marquise_dawkins@aol.com>
2023-01-02 22:47:12 -0500

    button creation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from 'react';
+import "./styles.css";
 
-const app = () => {
+function App(){
   return (
-    <div>
-    <h1>Marquise's Calulator</h1>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>x</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
     </div>
   )
 }
 
-export default app
+export default App
